278e28586c0238a486204a28927a70d9bba06577
BonkeyDonk <59089529+BonkeyDonk@users.noreply.github.com>
2021-10-30 09:31:34 +0300

    Added programs into the git

---
+++ Examap-combine.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+import os
+
+imconst = 3600
+
+final = Image.new("RGBA", (43200,21600), "white")
+
+for y in range(6):
+    for x in range(12):
+        name = str(y) + str(f"{x+1:02}") + ".png"
+        print("Processing: " + name)
+        currentImage = Image.open(name)
+        x_pos = imconst * x
+        y_pos = imconst * y
+        final.paste(currentImage,(x_pos,y_pos),currentImage)
+
+final.save("final.png")
---
+++ Examap-split.py
@@ -0,0 +1,25 @@
+from PIL import Image
+Image.MAX_IMAGE_PIXELS = 1000000000
+
+import os
+if not os.path.exists('examap_output'):
+    os.makedirs('examap_output')
+
+import os
+
+imconst = 3600
+
+input = Image.open("final.png")
+
+for y in range(6):
+    for x in range(12):
+        name = str(y) + str(f"{x+1:02}") + ".png"
+        print("Processing: " + name)
+        thisRegion = Image.new("RGBA", (3600,3600), "white")
+        left = imconst * x
+        top = imconst * y
+        right = imconst * x + imconst
+        bottom = imconst * y + imconst
+        cropped = input.crop((left, top, right, bottom))
+        thisRegion.paste(cropped,(0,0),cropped)
+        thisRegion.save("examap_output/" + name)
