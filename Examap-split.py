3d64feaa12e9d35f145439255e764d7f3dcda6a8
Chilneun <giaoxubomman@gmail.com>
2021-12-12 22:52:18 +0700

    added codes

--- Examap-split.py
+++ Examap-split.py
@@ -1,5 +1,5 @@
 from PIL import Image
-Image.MAX_IMAGE_PIXELS = 100000000000
+Image.MAX_IMAGE_PIXELS = 1000000000000000000000000000
 
 import os
 if not os.path.exists('examap_output'):
@@ -7,15 +7,13 @@ if not os.path.exists('examap_output'):
 
 import os
 
-end = input("What type of file do you want to save as? (ex: png) (Note: DO NOT ADD THE DOT!): ")
-
 imconst = 3600
 
 input = Image.open("final.png")
 
 for y in range(6):
     for x in range(12):
-        name = str(y) + str(f"{x+1:02}") + "." + end
+        name = str(y) + str(f"{x+1:02}") + ".png"
         print("Processing: " + name)
         thisRegion = Image.new("RGBA", (3600,3600), "white")
         left = imconst * x
